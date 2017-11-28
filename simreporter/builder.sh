429fce785bf05c42d85758b5433c73ed4490c890
Matt Clark <mattjclark0407@hotmail.com>
2017-11-28 12:50:41 +1100

    Check output of docker command before saying successful

--- simreporter/builder.sh
+++ simreporter/builder.sh
@@ -10,7 +10,9 @@ while true; do
             image=`ls -rt build/*.zip | tail -1`
             echo "`date`: Image is ${image}"
             docker cp build/${image} nmeaproxy:/tmp/firmware
-            echo "`date`: Build successful and image uploaded."
+            if [ $? -eq 0 ] ; then
+                echo "`date`: Build successful and image uploaded."
+            fi
         fi
     fi
     echo "`date`: Sleeping for 60 seconds"
