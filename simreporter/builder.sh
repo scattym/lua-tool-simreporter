231b501539ffc8f2e3d4dc1442d27b58df26755b
Matt Clark <mattjclark0407@hotmail.com>
2017-11-28 11:53:40 +1100

    Don't use intermediate variable.

--- simreporter/builder.sh
+++ simreporter/builder.sh
@@ -5,8 +5,7 @@ while true; do
         echo "`date`: Lua tool simreporter already up to date"
     else
         ./buildrelease.py -n
-        result=$?
-        if [ ${result} == 0 ] ; then
+        if [ $? -eq 0 ] ; then
             docker exec --user=root nmeaproxy bash -c 'rm /tmp/firmware/*.zip'
             image=`ls -rt build | tail -1`
             docker cp ${image} nmeaproxy:/tmp/firmware
