ab3b1ca5dad9e1bee93bfca0c3fe1b53cc306d90
Matt Clark <mattjclark0407@hotmail.com>
2017-11-28 11:27:53 +1100

    Stop the reset happening by default from the keep updated script.

--- simreporter/builder.sh
+++ simreporter/builder.sh
@@ -4,7 +4,7 @@ while true; do
     if git pull | grep -q "Already up-to-date" ; then
         echo "`date`: Lua tool simreporter already up to date"
     else
-        ./buildrelease.py
+        ./buildrelease.py -n
     fi
     echo "`date`: Sleeping for 60 seconds"
     sleep 60
