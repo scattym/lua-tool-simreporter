3081a4361d03fe3b2f726938b6a9704b46815be6
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2018-03-10 20:22:04 +1100

    Fix up build script

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -7,7 +7,8 @@ BB_AUTH_STRING=${BB_USER}:${BB_TICKET}
 cd simreporter
 python buildrelease.py -n
 if [ $? -eq 0 ] ; then
-    export image=`ls -rt build/*.zip | tail -1`
+    cd build
+    export image=`ls -rt *.zip | tail -1`
     echo "`date`: Image is ${image}"
     curl -X POST --user "${BB_AUTH_STRING}" "https://api.bitbucket.org/2.0/repositories/poslive/lua-tool-simreporter/downloads" --form files=@"${image}"
 fi
