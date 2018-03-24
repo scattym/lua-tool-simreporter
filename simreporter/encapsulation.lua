10e8e334b77203dbc2558df3c300adb0ca7df396
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2018-03-24 16:26:22 +1100

    Clean up encapsulation api.

--- simreporter/encapsulation.lua
+++ simreporter/encapsulation.lua
@@ -17,10 +17,6 @@ local add_field = function(data, key, value, type)
     else
         return_str = string.format("%s\"%s\"", return_str, value_str:gsub("\r\n", "|"):gsub('"', '\\"'))
     end;
-
-    --print("return string is:")
-    --print(return_str)
-    --print("\r\n")
     return return_str;
 end;
 
@@ -38,34 +34,8 @@ local encapsulate_data = function(device_info, table_data, iteration, count)
     table_data["device_info"] = device_info
     encode_strings(table_data)
     return json.encode(table_data)
-    --[[local data = ""
-    data = add_field(data, "version", "1", "raw")
-    data = add_field(data, "device_info", device_info, "string")
-    for key, value in pairs(table_data) do
-        data = add_field(data, key, value, "string")
-    end;
-    data = add_field(data, "packet_number", iteration, "raw")
-    data = add_field(data, "packet_count", count, "raw")
-    return "{" .. data .. "}";]]--
 end;
 
 _M.encapsulate_data = encapsulate_data
-
-
-
-local encapsulate_nmea = function(device_info, key, data, iteration, count)
-    local before = "{ \"version\" : 1, "
-    before = before .. "\"device_info\" : \"" .. tostring(device_info) .. "\", "
-    before = before .. "\"nmea_number\" : " .. tostring(iteration) .. ", "
-    before = before .. "\"nmea_total\" : " .. tostring(count) .. ", "
-    --print("Before is " .. before .. "\r\n")
-    local payload = "\"" .. key .."\" : \"" .. tostring(data) .. "\" "
-    --print("Payload is " .. payload .. "\r\n")
-    local after = "}"
-    --print("After is " .. after .. "\r\n")
-    return before .. payload .. after
-end
-
-_M.encapsulate_nmea = encapsulate_nmea
 
 return _M
